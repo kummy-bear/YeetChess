08ba9fc80aa1dbd64564d6c62ba5f61e63be4293
nammonman <sirapat69@outlook.com>
2023-09-27 10:13:30 +0700

    can i put ny balls in yo jawas
    balls in yo jaws

--- Assets/Scripts/PlaceOnPlane.cs
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -1,40 +1,43 @@
+
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-public class ARPrefabPlacement : MonoBehaviour
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTapToPlace : MonoBehaviour
 {
-    public GameObject prefabToSpawn;
-    private ARRaycastManager raycastManager;
-    private bool hasSpawned = false;
+    [SerializeField] private GameObject prefabObject;
 
-    private void Start()
+    private GameObject spawnedObject;
+    private Vector2 touchPosition;
+    private ARRaycastManager _arRaycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
     {
-        raycastManager = GetComponent<ARRaycastManager>();
+        _arRaycastManager = GetComponent<ARRaycastManager>();
     }
 
-    private void Update()
+    void Update()
     {
-        if (!hasSpawned && Input.touchCount > 0)
+        if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Began)
-            {
-                // Create a ray from the screen point
-                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                touchPosition = touch.position;
 
-                // Perform the raycast
-                if (raycastManager.Raycast(ray, s_Hits, TrackableType.PlaneWithinPolygon))
-                {
-                    Pose hitPose = s_Hits[0].pose;
-                    Instantiate(prefabToSpawn, hitPose.position, hitPose.rotation);
-                    hasSpawned = true;
-                }
+            if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = hits[0].pose;
+
+                if (spawnedObject == null)
+                    spawnedObject = Instantiate(prefabObject, hitPose.position, hitPose.rotation);
+                else
+                    spawnedObject.transform.position = hitPose.position;
             }
         }
     }
-
-    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
 }
