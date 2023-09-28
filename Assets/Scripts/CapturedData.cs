fe40d7dc0cc0c99d159383b74165ebb5f2413afd
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-09-28 15:26:46 +0700

    add stroage

---
+++ Assets/Scripts/CapturedData.cs
@@ -0,0 +1,5 @@
+public static class CaptureData
+{
+    public static List<GameObject> destroyedByWhite = new List<GameObject>();
+    public static List<GameObject> destroyedByBlack = new List<GameObject>();
+}
