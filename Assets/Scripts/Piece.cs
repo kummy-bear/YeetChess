3a3cd9383477677ae0e1008e92fe16b46d4eccba
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-09-30 12:27:09 +0700

    init class

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece
+{
+    public string Name { get; private set; }
+    public string Color { get; private set; }
+    public int Weight { get; private set; }
+
+    public Piece(string fullName)
+    {
+        fullName = fullName.split(" ");
+        Name = fullName[1];
+        Color = fullName[0];
+
+        if (name == "Rook")
+        {
+            Weight = 0;
+        }
+        if (name == "Pawn")
+        {
+            Weight = 0;
+        }
+        if (name == "Knight")
+        {
+            Weight = 0;
+        }
+        if (name == "Princess")
+        {
+            Weight = 0;
+        }
+        if (name == "Horse")
+        {
+            Weight = 0;
+        }
+        if (name == "King")
+        {
+            Weight = 0;
+        }
+    }
+}
