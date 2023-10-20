9189675b6486eb59ad2f9426d40bf01206a3fe15
Panyawat_KRONGKITICHU <pan.kkt@gmail.com>
2023-10-20 21:02:22 +0700

    pop up

--- Assets/Scripts/CollisionDetectionForBlackKing.cs
+++ Assets/Scripts/CollisionDetectionForBlackKing.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class CollisionDetectionForBlackKing : MonoBehaviour
 {
     public LayerMask collisionLayer;
+    [SerializeField] GameObject _wwin;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Throw"))
         {
             Debug.Log("Black king ded");
+            _wwin.SetActive(true);
         }
     }
 }
--- Assets/Scripts/CollisionDetectionForWhiteKing.cs
+++ Assets/Scripts/CollisionDetectionForWhiteKing.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class CollisionDetectionForWhiteKing : MonoBehaviour
 {
     public LayerMask collisionLayer;
+    [SerializeField] GameObject _bwin;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Throw"))
         {
             Debug.Log("White king ded");
+            _bwin.SetActive(true);
         }
     }
 }
